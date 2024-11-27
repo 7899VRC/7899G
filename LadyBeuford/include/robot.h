b819bee0379c664765265f2c695005d8597cecd6
7899VRC <138609260+7899VRC@users.noreply.github.com>
2024-11-26 18:23:03 -0800

    Ports
    changed ports

--- LadyBeuford/include/robot.h
+++ LadyBeuford/include/robot.h
@@ -35,24 +35,24 @@ brain Brain;
 controller Controller;
 
 // Left motors
-motor left_motor_front = motor(PORT10, ratio6_1, false);
+motor left_motor_front = motor(PORT8, ratio6_1, false);
 motor left_motor_middle = motor(PORT9, ratio6_1, true);
-motor left_motor_back = motor(PORT20, ratio6_1, true);
+motor left_motor_back = motor(PORT21, ratio6_1, true);
 
 // Right motors
-motor right_motor_front = motor(PORT1, ratio6_1, true);
-motor right_motor_middle = motor(PORT2, ratio6_1, false);
-motor right_motor_back = motor(PORT14, ratio6_1, false);
+motor right_motor_front = motor(PORT20, ratio6_1, true);
+motor right_motor_middle = motor(PORT18, ratio6_1, false);
+motor right_motor_back = motor(PORT17, ratio6_1, false);
 
 digital_out mogo_mech = digital_out(Brain.ThreeWirePort.A);
 
-motor hook = motor(PORT15, ratio6_1, true);
-motor LB = motor(PORT3, ratio36_1, true);
+motor hook = motor(PORT2, ratio6_1, false);
+motor LB = motor(PORT11, ratio36_1, true);
 
 
 
 inertial Inertial = inertial(PORT13);
-optical Optical = optical(PORT1);
+optical Optical = optical(PORT15);
 color detectedColor = Optical.color();
 rotation LBRotation = rotation(PORT2, false);
 
